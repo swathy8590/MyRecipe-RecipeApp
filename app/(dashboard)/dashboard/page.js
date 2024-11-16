89dd958221708c65ffbd0f8b17a546287a7eaff9
swathy8590 <swathyrajan674@gmail.com>
2024-11-16 20:17:16 +0530

    window

--- app/(dashboard)/dashboard/page.js
+++ app/(dashboard)/dashboard/page.js
@@ -4,6 +4,16 @@
 import Recipedetails from "@/app/components/common/recipedetail/Recipedetails";
 
 export default function Home() {
+    const [windowWidth, setWindowWidth] = useState(null);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const handleResize = () => setWindowWidth(window.innerWidth);
+            window.addEventListener('resize', handleResize);
+            handleResize(); // Initialize width on mount
+            return () => window.removeEventListener('resize', handleResize); // Cleanup on unmount
+        }
+    }, []);
 
 
     return (
